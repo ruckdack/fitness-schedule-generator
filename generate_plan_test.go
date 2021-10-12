c837efdd54460d862be818a30a2f48b5da760e24
Alexander Schmehl <alexander.s.schmehl@gmail.com>
2021-10-12 23:27:45 +0200

    created tests for 1RM calculations

--- generate_plan_test.go
+++ generate_plan_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 func TestSets(t *testing.T) {
 	configPlan := ReadJson("config.json")
@@ -24,6 +27,47 @@ func TestSets(t *testing.T) {
 		for muscle := range(haveSetsPerMuscle) {
 			if wantSetsPerMuscle[muscle] != haveSetsPerMuscle[muscle] {
 				t.Error(muscle + " is wrong")
+			}
+		}
+	}
+}
+
+func TestOneRM(t *testing.T) {
+	configPlan := ReadJson("config.json")
+	plan := GeneratePlan(configPlan)
+	for weekIdx, week := range(plan) {
+		for _, workoutDay := range(week) {
+			for _, exercise := range(workoutDay.Exercises) {
+				haveInitialOneRM := exercise.Weight * (1 + (exercise.Weight + float64(exercise.Reps)) / 30)
+				haveInitialOneRM /= math.Pow(1 + ONE_RM_INCREASE, float64(weekIdx % (len(plan)-1)))
+				wantInitialOnrRM := func() float64 {
+					for _, exercise := range(configPlan.Exercises) {
+						return exercise.InitialOneRM
+					}
+					return 0
+				}()
+				if (haveInitialOneRM - wantInitialOnrRM) < 0.25 {
+					t.Error(exercise.Name + " 1RM is not correct")
+				}
+			}
+		} 
+	}
+}
+
+func TestDeloadOneRM(t *testing.T) {
+	configPlan := ReadJson("config.json")
+	plan := GeneratePlan(configPlan)
+	for _, workoutDay := range(plan[len(plan)-1]) {
+		for _, exercise := range(workoutDay.Exercises) {
+			haveInitialOneRM := exercise.Weight * (1 + (exercise.Weight + float64(exercise.Reps)) / 30)
+			wantInitialOnrRM := func() float64 {
+				for _, exercise := range(configPlan.Exercises) {
+					return exercise.InitialOneRM
+				}
+				return 0
+			}()
+			if (haveInitialOneRM - wantInitialOnrRM) < 0.25 {
+				t.Error(exercise.Name + " 1RM is not correct")
 			}
 		}
 	}
