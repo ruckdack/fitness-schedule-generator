a9d1807f539d2a43835d4bbd95980e91a4b299b1
Alexander Schmehl <alexander.s.schmehl@gmail.com>
2021-10-13 22:16:20 +0200

    outsources pipe reading in main

--- main.go
+++ main.go
@@ -2,30 +2,39 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"io"
+	"log"
 	"os"
 )
 
 func main() {
+	jsonString, err := ReadPipe()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	configPlan := ReadJson(jsonString)
+	plan := GeneratePlan(configPlan)
+	fmt.Println(ConvertIntoPrettyJSON(plan))
+}
+
+func ReadPipe() (string, error) {
 	info, err := os.Stdin.Stat()
     if err != nil {
         panic(err)
     }
 	if info.Mode()&os.ModeCharDevice != 0 {
-		fmt.Println("Pipe something into this command")
-		return
+		return "", errors.New("pipe something into this command")
 	}
 	reader := bufio.NewReader(os.Stdin)
-	var jsonString string
+	var str string
 	for {
 		line, err := reader.ReadString('\n')
 		if err != nil && err == io.EOF {
 			break
 		}
-		jsonString += line
+		str += line
 	}
-	configPlan := ReadJson(jsonString)
-	plan := GeneratePlan(configPlan)
-	fmt.Println(ConvertIntoPrettyJSON(plan))
+	return str, nil
 }
